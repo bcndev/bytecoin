f90a87f7976ad92eb3ad71cbb344c7ad66f2843b
HomDx <uqgvhom3dxto@bk.ru>
2018-03-22 17:00:26 +0300

    Update make docker for release v.3.0.0

--- docker/docker1/src/make-bcn-docker.sh
+++ docker/docker1/src/make-bcn-docker.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+RESULT_MSG='Copy result build to save folder'
+
 git clone https://github.com/bcndev/bytecoin.git
 cd bytecoin
+git clone https://github.com/openssl/openssl.git
+cd openssl
+./Configure linux-x86_64 no-shared
+time make -j4
+echo 'Copy source openssl and compiled'
+cp ./include/openssl/* ./
+cp ./apps/openssl ./
+cd ..
+
 git clone https://github.com/LMDB/lmdb.git
 mkdir -p build
 cd build
 cmake ..
 time make -j4
+
+echo $RESULT_MSG
+cp -v ../bin/* /save
 
 cd ../..
 git clone https://github.com/bcndev/bytecoin-gui.git
@@ -15,5 +29,8 @@ mkdir -p build
 cd build
 cmake ..
 time make -j4
-cp -v ../../bytecoin/bin/* /save/
-cp -v ../bin/* /save/
+echo $RESULT_MSG
+cp -v ../bin/* /save
+
+echo Remove sources
+cd ../.. && rm -rf bytecoin-gui && rm -rf bytecoin-gui
